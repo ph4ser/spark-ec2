09c455861185d704fed642a1226012fe13101328
Jian <jian@Jians-MacBook-Pro.local>
2017-08-26 22:27:17 +0800

    update hadoop version

--- ephemeral-hdfs/init.sh
+++ ephemeral-hdfs/init.sh
@@ -17,17 +17,28 @@ case "$HADOOP_MAJOR_VERSION" in
     sed -i 's/-jvm server/-server/g' /root/ephemeral-hdfs/bin/hadoop
     cp /root/hadoop-native/* /root/ephemeral-hdfs/lib/native/
     ;;
-  2) 
-    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz  
+#  2) 
+#    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.0.0-cdh4.2.0.tar.gz  
+#    echo "Unpacking Hadoop"
+#    tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
+#    rm hadoop-*.tar.gz
+#    mv hadoop-2.0.0-cdh4.2.0/ ephemeral-hdfs/
+#
+#    # Have single conf dir
+#    rm -rf /root/ephemeral-hdfs/etc/hadoop/
+#    ln -s /root/ephemeral-hdfs/conf /root/ephemeral-hdfs/etc/hadoop
+#    cp /root/hadoop-native/* /root/ephemeral-hdfs/lib/native/
+#    ;;
+  2)
+    wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.7.0.tar.gz
     echo "Unpacking Hadoop"
     tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
-    mv hadoop-2.0.0-cdh4.2.0/ ephemeral-hdfs/
+    mv hadoop-2.7.0/ ephemeral-hdfs/
 
     # Have single conf dir
     rm -rf /root/ephemeral-hdfs/etc/hadoop/
     ln -s /root/ephemeral-hdfs/conf /root/ephemeral-hdfs/etc/hadoop
-    cp /root/hadoop-native/* /root/ephemeral-hdfs/lib/native/
     ;;
   yarn)
     wget http://s3.amazonaws.com/spark-related-packages/hadoop-2.4.0.tar.gz
